60440d458dff394e9c982357f2dc9445dd951f56
Rajesh-Sangepu <159781484+Rajesh-Sangepu@users.noreply.github.com>
2025-09-04 18:47:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,4 @@ fs.readFile('nonexistent.txt', 'utf8', (err, data) => {
 });
 
 // Intentional typo error
-consol.log("This line has a typo!"); // Should be console.log
-
-
-
-
-
+consol.log("This line has a typo!"); // Should be console.lo
