8d7d1399abe9aa53d73264ea2ac677e082742e1e
Rajesh-Sangepu <159781484+Rajesh-Sangepu@users.noreply.github.com>
2025-09-04 20:07:56 +0530

    Update myfile.java

--- myfile.java
+++ myfile.java
@@ -2,7 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {  // Corrected method signature
         System.out.println("Hello World");    // Added missing semicolon
         int number = 10;                       // Corrected type: int instead of String
-        System.out.println("Number is: " + number)
+        System.out.println("Number is: " + number);
     }
 }
 
