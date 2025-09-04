d80d1dfc93198e468ce8bb77932b5683136188ba
Rajesh-Sangepu <159781484+Rajesh-Sangepu@users.noreply.github.com>
2025-09-04 21:57:49 +0530

    Update ex.go

--- ex.go
+++ ex.go
@@ -1,4 +1,4 @@
-// file: error-sample.go
+/*// file: error-sample.go
 package main
 
 import "fmt"
@@ -17,4 +17,33 @@ func main() {
     // 4️⃣ Logical runtime error (will compile but panic)
     arr := []int{1, 2, 3}
     fmt.Println(arr[5]) // out of range
+}
+*/
+
+
+// file: error-sample.go
+package main
+
+import "fmt"
+
+func main() {
+    // 1️⃣ Fixed syntax error: added closing parenthesis
+    fmt.Println("Hello, World!")
+
+    // 2️⃣ Fixed undefined variable by defining it
+    nonExistentVar := "I exist now"
+    fmt.Println(nonExistentVar)
+
+    // 3️⃣ Fixed type error by converting int to string properly
+    var num int = 10
+    str := fmt.Sprintf("%d", num)  // convert int to string
+    fmt.Println(str)
+
+    // 4️⃣ Fixed logical runtime error by accessing valid index
+    arr := []int{1, 2, 3}
+    if len(arr) > 5 {
+        fmt.Println(arr[5])
+    } else {
+        fmt.Println("Index 5 is out of range. Array length is", len(arr))
+    }
 }
